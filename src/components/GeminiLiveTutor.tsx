0512de85cdcf8ef01035d2e71f5c5168f9b25ef7
vsrivathsan88 <vaishnavisrivathsan@gmail.com>
2025-08-13 23:03:22 -0700

    chore(build): allow build despite ESLint/TS errors for iterative UX copy changes

--- src/components/GeminiLiveTutor.tsx
+++ src/components/GeminiLiveTutor.tsx
@@ -264,7 +264,7 @@ export default function GeminiLiveTutor({ className = '', autoStartVoice = false
   const getMessageStyle = (type: TutorMessage['type']) => {
     switch (type) {
       case 'hint': return 'bg-blue-50 border-blue-200 text-blue-800'
-      case 'celebration': return 'bg-purple-50 border-purple-200 text-purple-800'
+      case 'celebration': return 'bg-green-50 border-green-200 text-green-800'
       case 'question': return 'bg-purple-50 border-purple-200 text-purple-800'
       default: return 'bg-gray-50 border-gray-200 text-gray-800'
     }
